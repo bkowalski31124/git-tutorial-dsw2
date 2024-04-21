bb701eb447eec46273c48d94bb423c452f4100d1
Bartosz <dsw52715@dsw.student.edu.pl>
2024-04-21 23:09:50 +0200

    Dodano tworzenie 100 plikówz nazwą, nazwą skryptu i datą

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,13 @@
 
 dzisiejsza_data=$(date +"%d-%m-%Y")
 echo "Dzisiejsza data: $dzisiejsza_data"
+
+for ((i=1; i<=100; i++))
+do
+	filename="log${i}.txt"
+	touch "$filename"
+	echo "$filename" >> "$filename"
+	echo "$(basename "$0")" >> "$filename"
+        echo "$dzisiejsza_data" >> "$filename"
+done
+
